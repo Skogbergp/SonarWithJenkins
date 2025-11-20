c80250c5f8c8ab146475c7ff20ad976654ee98c3
Patrik Skogberg <skogbergp@gmail.com>
2025-11-20 13:45:32 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        loopNumbers(askAmount());
+    }
+    public static void loopNumbers(int loopAmount){
+        for (int i = 1; i <= loopAmount; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+    public static int askAmount(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("how many numbers to display in the loops?");
+        while (true) {
+            if (input.hasNextInt()) {
+                return input.nextInt();
+            }
+        }
+    }
+}
